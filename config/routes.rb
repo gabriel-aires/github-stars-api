20c0692c340ac5c191165ba6fb4aed2c2cdf13f0
Gabriel Aires Guedes <airesgabriel@gmail.com>
2024-06-29 17:31:13 -0300

    add routes for creating users and listing repositories

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # create user
+  post "users/:login", to: "users#create"
+
+  # show repositories
+  get "users/:login/repositories", to: "repositories#user_repositories"
 end
