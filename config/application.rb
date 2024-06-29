5f48839bf48a32613ef2a7e85e78cf5fc81d11f4
Gabriel Aires Guedes <airesgabriel@gmail.com>
2024-06-29 15:49:46 -0300

    set active job backend

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module GithubStarsApi
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    # Set Active Job backend
+    config.active_job.queue_adapter = :sucker_punch
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
